90c0b516b932fc648089e278d2d5fccfdcd06b18
Supratim Chowdhury <supratims@gmail.com>
2016-08-05 22:34:52 +0100

    initial commit

---
+++ Login.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'bootstrap.php';
+
+class Login {
+
+	public function authenticate($login, $password){
+
+	}
+
+	public function register($login, $name, $email, $password){
+
+	}
+
+	public function isAdmin(){
+
+	}
+}
