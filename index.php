e6922863b090505b552ccaa04dde8781e9d2dba5
Supratim Chowdhury <supratims@gmail.com>
2016-08-05 22:34:45 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-require_once 'bootstrap.php';
-use M1ke\Sql\ExtendedPdo;
 
-$pdo = new ExtendedPdo('test', 'root', 'root');
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+	$response->write("Hello, " . $args['name']);
+	return $response;
+});
+
+$app->run();
